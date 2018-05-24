11eaa77e4a79f65dc6fac5510bf51eeed9310ac6
andresshare <andresshare@gmail.com>
2018-05-23 22:57:09 -0500

    templates

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 from todo_list import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todo_list.urls')),
+    path(' ', include('todo_list.urls')),
 ]
